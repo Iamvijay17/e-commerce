778d4012d013e9c1e0dab74afde2ad4f86102b75
Iamvijay17 <vijay9578979255@gmail.com>
2023-11-13 16:39:32 +0530

    Add copyright

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className='footer text-center'>
       
       <img src="https://picsxtra.com/images/2023/11/13/footer.jpg" alt="" />
-      <p>© 2023 Vijay India Pvt Ltd. All rights reserved.</p>
+      
     </div>
       </div>
       <Footer1/>
--- src/Components/Footer/Footer1.jsx
+++ src/Components/Footer/Footer1.jsx
@@ -2,33 +2,33 @@ import React from 'react'
 import './Footer1.css'
 
 const Footer1 = () => {
-  return (
-    <div>
-      <footer class="footer-area footer--light">
+    return (
+        <div>
+            <footer class="footer-area footer--light">
                 <div class="footer-big">
-                  
+
                     <div class="container">
                         <div class="row">
                             <div class="col-md-3 col-sm-12">
                                 <div class="footer-widget">
                                     <div class="widget-about">
-                                        <img className=' rounded-3' src="https://global-uploads.webflow.com/5ca5fe687e34be0992df1fbe/60bc8d1c8c29c441712b2931_MOUNTAIN%20comp%20-%20021%20---%20Expires%20on%2017-07-2021-min-p-1080.jpeg" alt="" class="img-fluid"/>
-                                            <p className='fp'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, magnam? </p>
-                                            <ul class="contact-details">
-                                                <li>
-                                                    <span class="icon-earphones"></span> Call Us:
-                                                    <a href="tel:344-755-111">+91 98722 78967</a>
-                                                </li>
-                                                <li>
-                                                    <span class="icon-envelope-open"></span>
-                                                    <a href="mailto:support@Fashionpass.com">support@Fashionpass.com</a>
-                                                </li>
-                                            </ul>
+                                        <img className=' rounded-3' src="https://global-uploads.webflow.com/5ca5fe687e34be0992df1fbe/60bc8d1c8c29c441712b2931_MOUNTAIN%20comp%20-%20021%20---%20Expires%20on%2017-07-2021-min-p-1080.jpeg" alt="" class="img-fluid" />
+                                        <p className='fp'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, magnam? </p>
+                                        <ul class="contact-details">
+                                            <li>
+                                                <span class="icon-earphones"></span> Call Us:
+                                                <a href="tel:344-755-111">+91 98722 78967</a>
+                                            </li>
+                                            <li>
+                                                <span class="icon-envelope-open"></span>
+                                                <a href="mailto:support@Fashionpass.com">support@Fashionpass.com</a>
+                                            </li>
+                                        </ul>
                                     </div>
                                 </div>
-                               
+
                             </div>
-                           
+
                             <div class="col-md-3 col-sm-4">
                                 <div class="footer-widget">
                                     <div class="footer-menu footer-menu--1">
@@ -51,11 +51,11 @@ const Footer1 = () => {
                                             </li>
                                         </ul>
                                     </div>
-                                  
+
                                 </div>
-                                
+
                             </div>
-                           
+
 
                             <div class="col-md-3 col-sm-4">
                                 <div class="footer-widget">
@@ -85,11 +85,11 @@ const Footer1 = () => {
                                             </li>
                                         </ul>
                                     </div>
-                                 
+
                                 </div>
-                               
+
                             </div>
-                            
+
 
                             <div class="col-md-3 col-sm-4">
                                 <div class="footer-widget">
@@ -119,23 +119,23 @@ const Footer1 = () => {
                                             </li>
                                         </ul>
                                     </div>
-                                    
+
                                 </div>
-                               
+
                             </div>
-                           
+                            <p className='text-center'>© 2023 Vijay India Pvt Ltd. All rights reserved.</p>
+
 
                         </div>
-                        
-                    </div>
-                   
-                </div>
-                
 
-               
+                    </div>
+                </div>
+
+
+
             </footer>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Footer1
