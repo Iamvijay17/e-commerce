dd6ef966aca64e4ed5b60f39ef6d9ba913c8a7ec
Iamvijay17 <vijay9578979255@gmail.com>
2023-11-14 07:54:47 +0530

    Fix some err

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -49,7 +49,7 @@ const Item = (props) => {
         </div>
         <div className="cta row">
           <button className="btn btn-primary col-6 btn-cart shadow-none" onClick={() => { addToCart(props.id) }}>Add to Card</button>
-          <Link to='./cart'><button className="btn btn-primary col-6 btn-buy shadow-none " onClick={() => { addToCart(props.id) }}>Buy Now</button></Link>
+          <Link to='/cart'><button className="btn btn-primary col-6 btn-buy shadow-none " onClick={() => { addToCart(props.id) }}>Buy Now</button></Link>
         </div>
       </div>
     </div>
